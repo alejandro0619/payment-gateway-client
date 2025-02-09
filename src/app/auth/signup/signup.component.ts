4f1350db570d10dfba5574bc89a68a984545dba5
Alejandrolpz0619@gmail.com <Alejandrolpz0619@gmail.com>
2025-02-09 19:11:27 -0400

    feat: enhance signup form with additional validation messages and improve layout

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,31 +1,36 @@
-import { Component, inject, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './signup.component.html',
-
 })
 export class SignupComponent {
-  form = signal(new FormGroup({
+  form = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    lastName: new FormControl('', [Validators.required, Validators.minLength(3)]),
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(8)]),
     confirmPassword: new FormControl('', [Validators.required]),
-    country: new FormControl('', [Validators.required]),
-    state: new FormControl('', [Validators.required]),
-    city: new FormControl('', [Validators.required]),
-  }));
+  }, { validators: this.passwordsMatchValidator() }); 
 
-  get passwordsMatch() {
-    return this.form().value.password === this.form().value.confirmPassword;
+
+  passwordsMatchValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const formGroup = control as FormGroup; 
+      const password = formGroup.get('password')?.value;
+      const confirmPassword = formGroup.get('confirmPassword')?.value;
+      return password === confirmPassword ? null : { passwordsMismatch: true };
+    };
   }
 
   submit() {
-    if (this.form().valid && this.passwordsMatch) {
-      console.log('Form Submitted', this.form().value);
+    if (this.form.valid) {
+      console.log('Form Submitted', this.form.value);
+    } else {
+      console.log('Form is invalid');
     }
   }
 }
