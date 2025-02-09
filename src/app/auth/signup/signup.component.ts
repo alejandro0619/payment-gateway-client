096a408d376bcbde358199aeb816ede248660355
Alejandrolpz0619@gmail.com <Alejandrolpz0619@gmail.com>
2025-02-09 15:17:21 -0400

    feat: implement signup form with reactive validation and Tailwind CSS styling

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './signup.component.html',
 
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = signal(new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    confirmPassword: new FormControl('', [Validators.required]),
+    country: new FormControl('', [Validators.required]),
+    state: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required]),
+  }));
+
+  get passwordsMatch() {
+    return this.form().value.password === this.form().value.confirmPassword;
+  }
+
+  submit() {
+    if (this.form().valid && this.passwordsMatch) {
+      console.log('Form Submitted', this.form().value);
+    }
+  }
+}
